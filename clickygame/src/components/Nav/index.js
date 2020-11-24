bd02402df73060cee984803490859b588e0b994e
Mone E <mone.eunique12@gmail.com>
2020-11-23 19:13:06 -0800

    adding nav to app

--- clickygame/src/components/Nav/index.js
+++ clickygame/src/components/Nav/index.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./index.css";
 
 const NavBar = ({score,topScore}) => {
     return (
         <>
-            <nav className = "navbar navbar-light bg-light">
-            <div className = "col-md-4 center">Clicky Game</div>
-            <div className = "col-md-4 center">Click any image to begin!</div>
-            <div className = "col-md-4 center">Score:{score} Top Score:{topScore}</div>
-            
+            <nav className = "navbar navbar-light bg-dark justify-content-center">
+            <ul className = "nav col-md-12">
+             <li className = "nav-item col-md-4">Clicky Game</li>
+             <li className = "nav-item col-md-4">Click any image to begin!</li>
+             <li className = "nav-item col-md-4">Score:{score} Top Score:{topScore}</li>
+            </ul>
             </nav>
 
         </>
