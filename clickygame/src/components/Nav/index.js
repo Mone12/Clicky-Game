03ae3ab306934ea869d2e4b66161ae4d69bdb6f5
Mone E <mone.eunique12@gmail.com>
2020-12-14 22:40:27 -0800

    fixed score function

--- clickygame/src/components/Nav/index.js
+++ clickygame/src/components/Nav/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./index.css";
 
-const NavBar = ({score,topScore}) => {
+const NavBar = (props) => {
     return (
         <>
             <nav className = "shadow navbar navbar-light bg-dark justify-content-center">
             <ul className = "nav col-md-12">
              <li className = "nav-item col-md-4">Clicky Game</li>
              <li className = "nav-item col-md-4">Click any image to begin!</li>
-             <li className = "nav-item col-md-4">Score:{score} Top Score:{topScore}</li>
+             <li className = "nav-item col-md-4">Score:{props.current} Top Score:{props.high}</li>
             </ul>
             </nav>
 
