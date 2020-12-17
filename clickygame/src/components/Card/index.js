a2fd6d341df72801a0b45a631adf99d3478e3a5c
Mone E <mone.eunique12@gmail.com>
2020-12-16 21:57:40 -0800

    css styling

--- clickygame/src/components/Card/index.js
+++ clickygame/src/components/Card/index.js
@@ -4,7 +4,7 @@ import './index.css';
 function Card(props) {
   return (
     <div className="container col-md-3">
-    <div className="card">
+    <div className="card rounded">
     <img alt={props.name} src={props.image} id = {props.id} onClick={props.handleIncrement}/>
         
      
--- clickygame/src/components/Nav/index.js
+++ clickygame/src/components/Nav/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./index.css";
+import img from "./SMlogo2.png";
 
 const NavBar = (props) => {
     return (
         <>
             <nav className = "shadow navbar fixed-top navbar-light bg-dark justify-content-center">
             <ul className = "nav col-md-12">
-             <li className = "nav-item col-md-4">Clicky Game</li>
-             <li className = "nav-item col-md-4">Click any image to begin!</li>
-             <li className = "nav-item col-md-4">Score:{props.current} Top Score:{props.high}</li>
+             <li className = "nav-item col-md-4 title">
+                <img className= "nav-logo" src = {img} alt = {props}/>
+                 Clicky Game
+            </li>
+             <li className = "nav-item col-md-4 list-position">Click any image to begin!</li>
+             <li className = "nav-item col-md-4 list-position">Score:{props.current} Top Score:{props.high}</li>
             </ul>
             </nav>
 
