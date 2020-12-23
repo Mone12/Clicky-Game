c58ff61fe07e969ac99a89f6af8021576e84433f
Mone E <mone.eunique12@gmail.com>
2020-12-22 16:18:42 -0800

    cleaning code

--- clickygame/src/components/Card/index.js
+++ clickygame/src/components/Card/index.js
@@ -1,16 +1,19 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 
 function Card(props) {
   return (
     <div className="container col-md-3">
-    <div className="card rounded">
-    <img alt={props.name} src={props.image} id = {props.id} onClick={props.handleIncrement}/>
-        
-     
+      <div className="card rounded">
+        <img
+          alt={props.name}
+          src={props.image}
+          id={props.id}
+          onClick={props.handleIncrement}
+        />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Card;
--- clickygame/src/components/Jumbotron/index.js
+++ clickygame/src/components/Jumbotron/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./index.css";
-import logo from  "./SMlogo.png";
+import logo from "./SMlogo.png";
 
-function Header (props) {
-    return (
-        <div className = "jumbotron jumbotron-fluid">
-            <span>
-                
-              <img className = "justify-content-center logo"src = {logo} alt = {props} />
+function Header(props) {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <span>
+        <img className="justify-content-center logo" src={logo} alt={props} />
 
-              <h4 className = 'jumbo-text text-white'>Click on an image to earn point,
-               but don't click on any more than once!</h4>
-               
-            </span>
-
+        <h4 className="jumbo-text text-white">
+          Click on an image to earn point, but don't click on any more than
+          once!
+        </h4>
+      </span>
     </div>
-    )
-};
+  );
+}
 
 export default Header;
